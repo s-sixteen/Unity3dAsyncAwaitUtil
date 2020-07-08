564ca8e143139f1f3bc07ce53653fe9ef92b42c8
Marek Mrva <mrvamarek@yahoo.com>
2020-07-08 18:48:31 +0200

    Fix async callbacks throwing exceptions on Editor assembly reload

--- UnityProject/Assets/Plugins/AsyncAwaitUtil/Source/Internal/SyncContextUtil.cs
+++ UnityProject/Assets/Plugins/AsyncAwaitUtil/Source/Internal/SyncContextUtil.cs
@@ -10,6 +10,9 @@ namespace UnityAsyncAwaitUtil
 {
     public static class SyncContextUtil
     {
+#if UNITY_EDITOR
+        [UnityEditor.Callbacks.DidReloadScripts]
+#endif
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         static void Install()
         {
